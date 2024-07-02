9b0de960817f32306b246e8d587539b755747384
MohamedEhabBasha <mohedehab530@gmail.com>
2024-07-02 18:54:36 +0300

    CompleteBasketAPI

--- eshop-microservices/Services/Basket/Basket.API/Program.cs
+++ eshop-microservices/Services/Basket/Basket.API/Program.cs
@@ -19,8 +19,19 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+     .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
@@ -29,5 +40,11 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(opts => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
